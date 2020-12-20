afcaf7c79b4a66c82405a1e7954602eaf6442e31
scabbucoral <2647862942@qq.com>
2020-12-20 15:09:48 +0800

    create 22.cpp
    create 22.cpp

---
+++ 22/22.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        string s;
+		vector<string> vec;
+		dfs(n, n, vec, s);
+		return vec;
+	}
+
+	void dfs(int left, int right, vector<string> &vec, string s) {
+		if (left < 0 || right < 0 || left >  right) {
+			return;
+		}
+		if (left == 0 && right == 0) {
+			vec.push_back(s);
+			return;
+		}
+
+		s.push_back('(');
+		dfs(left - 1, right, vec, s);
+		s.pop_back();
+		s.push_back(')');
+		dfs(left, right - 1, vec, s);
+		//s.pop_back();
+
+    }
+};
