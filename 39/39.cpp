a1c62aa959f0711f72401961ac517ebfbb036bf5
scabbucoral <2647862942@qq.com>
2020-12-28 16:01:08 +0800

    create 39.cpp

---
+++ 39/39.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> mid_vector;
+        vector<vector<int>> result;
+        solve(candidates, 0, 0, target, mid_vector, result);
+        return result;
+}
+
+    void solve(vector<int>& candidates, int mid_target, int index, int target, vector<int>& mid_vector, vector<vector<int>>& result) {
+        if (mid_target == target) {
+            result.push_back(mid_vector);
+            return;
+        }
+        if (mid_target > target) {
+            return;
+        }
+        for (int i = 0; i < candidates.size(); i++) {
+            mid_vector.push_back(candidates[i]);
+            //mid_target+=candidates[i];
+            solve(candidates, mid_target + candidates[i], i, target, mid_vector, result);
+            //solve(candidates, mid_target, i, target, mid_vector, result);
+            mid_vector.pop_back();
+        }
+    }
+};
